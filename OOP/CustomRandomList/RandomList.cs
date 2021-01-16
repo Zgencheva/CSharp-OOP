d13792ffb2f5e8a2d1049e6680c73beb9aa5a2a3
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2021-01-16 21:41:57 +0200

    CustomRandomList

---
+++ OOP/CustomRandomList/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+
+        public Random Rand { get; set; }
+
+        public RandomList()
+        {
+            this.Rand = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = Rand.Next(0, this.Count);
+            string output = this[index];
+            this.RemoveAt(index);
+            return output;
+        }
+    }
+}
