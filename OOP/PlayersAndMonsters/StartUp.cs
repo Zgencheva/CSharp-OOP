4e3f84fb30ea12d7f973f654d0eae9a0fdab0961
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2021-01-18 16:09:41 +0200

    Inheritance Exercise

---
+++ OOP/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DarkKnight darkNight = new DarkKnight("knighti", 5);
+            Console.WriteLine(darkNight);
+        }
+    }
+}
---
+++ OOP/Zoo/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Snake snake = new Snake("snake1");
+        }
+    }
+}
