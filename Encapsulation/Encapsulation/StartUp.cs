bcac3abd456472fe5f1aea868e240c8c3240cfc5
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2021-01-20 16:22:50 +0200

    Encapsulation SalaryIncrease and Validation lab

--- Encapsulation/Encapsulation/StartUp.cs
+++ Encapsulation/Encapsulation/StartUp.cs
@@ -8,21 +8,34 @@ namespace PersonsInfo
     {
         static void Main(string[] args)
         {
-            
+
             var lines = int.Parse(Console.ReadLine());
             var persons = new List<Person>();
             for (int i = 0; i < lines; i++)
             {
-                var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
-                //person.Age = 18; - inaccessible, because of the private setter
-                persons.Add(person);
-            }
+                try
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+                    var person = new Person(cmdArgs[0],
+                                            cmdArgs[1],
+                                            int.Parse(cmdArgs[2]),
+                                            decimal.Parse(cmdArgs[3]));
 
-            persons.OrderBy(p => p.FirstName)
-                   .ThenBy(p => p.Age)
-                   .ToList()
-                   .ForEach(p => Console.WriteLine(p.ToString()));
+                    persons.Add(person);
+                }
+                catch (ArgumentException ae)
+                {
+
+                    Console.WriteLine(ae.Message); ;
+                }
+                
+
+               
+            }
+            var parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
 
         }
     }
