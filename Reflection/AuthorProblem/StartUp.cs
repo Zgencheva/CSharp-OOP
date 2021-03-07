7ec0dd1880bfd5e06e79dde3460b6c107c05ce6f
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2021-03-07 17:25:30 +0200

    Reflection AuthorProblem

---
+++ Reflection/AuthorProblem/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [AuthorAttribute("Ventsy")]
+    public class StartUp
+    {
+        [AuthorAttribute("Gosho")]
+        public static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+
+        }
+    }
+}
